79cc5f07b9ba976b5d64a2a04962d90273816133
khaalid cabdiraxin <qaalidyare837@gmail.com>
2024-07-24 20:15:24 +0300

    Update

--- app/home/OurTeam.jsx
+++ app/home/OurTeam.jsx
@@ -13,6 +13,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+// Team Data
 const teamData = [
   {
     name: "Abdinour",
